a034f91786d44e6369c33edf5729d378b6337fd5
SGtOriginal <63638171+SGtOriginal@users.noreply.github.com>
2021-07-01 11:46:33 +0530

    Update discord-bot.py

--- discord-bot.py
+++ discord-bot.py
@@ -1,26 +1,17 @@
+import random
 import discord
-#from discord.ext.commands import Bot
 from discord.ext import commands
-from random import choice
-import shutil
-from discord.utils import get
-import os
-from os import system
 import asyncio
+from keep_alive import keep_alive
 
-client = commands.Bot(command_prefix = 'Put your bot prefix here')
+client = commands.Bot(command_prefix = 'Your Preferred Bot Prefix')
 
 @client.event
 async def on_ready():
-    print('Booted up...................\nUploading files to remote server............\nFiles upload complete!\nBot ready to start!\nStarting bot in.....5.....4.....3.....2.....1......0\nBot started!!!!!')
-
-@client.event
-async def on_member_join(member):
-    print(f'Hello, {member}, we hope you have a good time!')
-
-@client.event
-async def on_member_remove(member):
-    print(f'{member} has left. Bye!')
+    await client.change_presence(activity=discord.Game('Enter Any Random Here'))
+    
+    print('Connected to bot: {}'.format(client.user.name))
+    print('Bot ID: {}'.format(client.user.id))
 
 @client.command(
 	help="Uses come crazy logic to determine if pong is actually the correct value or not.",
@@ -36,43 +27,68 @@ async def hello(ctx):
 
 @client.command()
 @commands.has_permissions(manage_messages = True)
-async def clear(ctx):
-    await ctx.channel.purge(limit=10000)
+async def clear(ctx, a: int):
+    await ctx.channel.purge(limit=1 + a)
     await ctx.send(f'Channel cleared!')
 
 @client.command(
-	help="Changes the nickname to 'You got trolled'",
+	help="Changes the nickname.",
 	brief="The name says it all."
 )
-async def troll(ctx, member : discord.Member):
-    await member.edit(nick="You got trolled!")
+async def nick(ctx, member : discord.Member, args):
+  
+  await member.edit(nick=args)
     
+  await ctx.send(f'Nickname changed.')
+	
 @client.command(
-	help="Add numbers.",
-	brief="Add numbers."
+	help="Looks like you need some help, lol.",
+	brief="Prints the list of values back to the channel."
+)
+async def print(ctx, *args):
+	response = ""
+
+	for arg in args:
+		response = response + " " + arg
+
+	await ctx.channel.send(response)
+
+@client.command(
+	help="Add numbers like this 'add 10 20'.",
+	brief="Add numbers like this 'add 10 20'."
 )
 async def add(ctx, a: int, b: int):
 	await ctx.send(a + b)
 	
 @client.command(
-	help="Subtract numbers.",
-	brief="Subtract numbers."
+	help="Subtract numbers like this 'sub 10 20'.",
+	brief="Subtract numbers like this 'sub 10 20'."
 )
 async def sub(ctx, a: int, b: int):
 	await ctx.send(a - b)
 	
 @client.command(
-	help="Multiplies numbers.",
-	brief="Multiplies numbers."
+	help="Multiplies numbers like this 'mul 10 20'.",
+	brief="Multiplies numbers like this 'mul 10 20'."
 )
 async def mul(ctx, a: int, b: int):
 	await ctx.send(a * b)
 
 @client.command(
-	help="Divide numbers like this.",
-	brief="Divide numbers like this."
+	help="Divide numbers like this 'div 10 20'.",
+	brief="Divide numbers like this 'div 10 20'."
 )
 async def div(ctx, a: int, b: int):
 	await ctx.send(a / b)
-  
-client.run('Paste bot token here')
+
+@client.command(
+        help="Chooses between two options.",
+        brief="Chooses between two options."
+)
+async def choose(ctx, a: str, b: str):
+        list1 = [a, b]
+        await ctx.send(random.choice(list1))
+
+keep_alive()
+
+client.run('Paste Bot Token Here')
